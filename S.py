bdb82ae4a9e6a50d395c051ffcf7edf44de8271f
acmerfight <acmerfight@gmail.com>
2018-09-24 16:14:31 +0800

    add

---
+++ S.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+
+
+class Question(object):
+
+    def __init__(self, question_id, data_formatting):
+        self._question_id = question_id
+        self._data_formatting = data_formatting
+
+    def get(self):
+        # connect_MySQL
+        # get_data_from_MySQL
+        # connect_Redis
+        # get_data_from_Redis
+        return self._format_data()
+
+    def _format_data(self):
+        # 50 行代码
+        # bulalala
+        # bulalala
+        # bulalala
+        # bulalala
+        # bulalala
+        # bulalala
+        # bulalala
+        # bulalala
+        # bulalala
+        # bulalala
+        pass
