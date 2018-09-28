cabe33545413972d3a0b42ef48e96e8298c9958d
acmerfight <acmerfight@gmail.com>
2018-09-28 14:32:53 +0800

    add_max_execution_time

--- encapsulation.py
+++ encapsulation.py
@@ -20,22 +20,32 @@ class Person(object):
             return today.year - self.birth_day.year - 1
 
 
-birth_day = date(1989, 1, 1)
-sex = True
-children = []
-person = Person(birth_day, sex, children)
-
-person.age = 10
-
-# case one
-person.children = [1]
-person.sex = False
 
 
-# case two
-birth_day = person.birth_day
-birth_day.replace(year=2000)
-print(birth_day)
 
-# case three
-# person.compute_age()
+
+
+
+
+
+
+#
+# birth_day = date(1989, 1, 1)
+# sex = True
+# children = []
+# person = Person(birth_day, sex, children)
+#
+# person.age = 10
+#
+# # case one
+# person.children = [1]
+# person.sex = False
+#
+#
+# # case two
+# birth_day = person.birth_day
+# birth_day.replace(year=2000)
+# print(birth_day)
+#
+# # case three
+# # person.compute_age()
